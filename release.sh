5dff551af12f5c13068eb44da97e0b967d514f30
arirux <fdopen@ml.ignorelist.com>
2015-03-05 14:37:17 +0100

    release.sh fix

--- release.sh
+++ release.sh
@@ -3,23 +3,43 @@
 set -e
 set -u
 
-curdir="$(dirname "$0")"
+curdir="$(readlink -f "$0")"
+curdir="$(dirname "$curdir")"
 cd "$curdir"
 omake distclean
 omake setup.ml src/configure
 pkg="$(omake -s echo-pkg)"
-rm -rf src/autom4te.cache
 find . -type f \( -name '*~' -o -name '*.omc' -o -name '.omakedb*' \) -delete
 
-mtmpf="$(mktemp)"
-trap "rm -f \"${mtmpf}\"" EXIT
+mtmpf="$(mktemp -d)"
+trap "rm -rf \"${mtmpf}\"" EXIT
 
 if which gtar >/dev/null 2>&1 ; then
     tar=gtar
 else
     tar=tar
 fi
-$tar --transform "s|^.|${pkg}|" --format=ustar --numeric-owner -cf "$mtmpf" .
 
-xz -9e -k "$mtmpf" -c > "${pkg}.tar.xz"
-gzip -9 -k "$mtmpf" -c > "${pkg}.tar.gz"
+stash="$(git stash create)"
+git archive --format=tar ${stash:-HEAD} | ( cd "$mtmpf" ; tar -xf- )
+
+cp -p src/config.h.in src/configure "${mtmpf}/src"
+cp -p setup.ml _oasis "$mtmpf"
+
+if which gfind >/dev/null 2>&1 ; then
+    find=gfind
+else
+    find=find
+fi
+
+$find "$mtmpf" -type f ! -executable ! -perm 644 -exec chmod 644 {} \+
+$find "$mtmpf" -type f -executable ! -perm 755 -exec chmod 755 {} \+
+$find "$mtmpf" -type d ! -perm 755 -exec chmod 755 {} \+
+
+cd "$mtmpf"
+
+$tar --transform "s|^.|${pkg}|" --format=ustar --numeric-owner -cf- . | \
+    gzip -9 > "${curdir}/${pkg}.tar.gz"
+
+omake all >/dev/null 2>&1
+omake test
