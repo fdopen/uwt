bc70cc4f5e4fe0a5f4e2cedbbf392070db0894ec
fdopen <andreashauptmann@t-online.de>
2015-06-22 16:45:41 +0200

    add libuv build instructions

---
+++ libuv-compile.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+set -eu
+
+version=$1
+CC=$2
+CFLAGS=$3
+unix=$4
+
+archive="libuv-v${version}.tar.gz"
+archive_url="http://libuv.org/dist/v${version}/${archive}"
+
+if [ ! -f "$archive" ]; then
+    if which wget >/dev/null 2>&1 ; then
+        wget --quiet "$archive_url" -O "$archive"
+    else
+        curl -f -L --silent "$archive_url" -o "$archive"
+    fi
+fi
+
+tar -xzf "$archive"
+cd "libuv-v${version}"
+
+if [ "$unix" = "true" ]; then
+    x="$(uname)"
+    if [ "$x" = "OpenBSD" ] ; then
+        AUTOCONF_VERSION="$( ls -1 /usr/local/bin/autoreconf-* | sort | tail -n 1 )"
+        AUTOCONF_VERSION="${AUTOCONF_VERSION##*-}"
+
+        AUTOMAKE_VERSION="$( ls -1 /usr/local/bin/automake-* | sort | tail -n 1 )"
+        AUTOMAKE_VERSION="${AUTOMAKE_VERSION##*-}"
+
+        export AUTOCONF_VERSION AUTOMAKE_VERSION
+    fi
+
+    ./autogen.sh
+    ./configure --enable-static --disable-shared CC="$CC" CFLAGS="$CFLAGS"
+
+    make=gmake
+    if ! which gmake >/dev/null 2>&1 ; then
+        make=make
+    fi
+    $make all
+    cd ..
+    lib="../libuv-v${version}/.libs/libuv.a"
+    rm -f src/libuv.a examples/libuv.a test/libuv.a
+    ln -s "${lib}" src/libuv.a
+    ln -s "${lib}" examples/libuv.a
+    ln -s "${lib}" test/libuv.a
+else
+    AR=
+    case "$CC" in
+        *-gcc)
+            AR=${CC%-*}
+            AR="${AR}-ar"
+            if which "$AR" >/dev/null 2>&1 ; then
+               export AR
+            fi
+            ;;
+    esac
+    export CC CFLAGS
+    make -f Makefile.mingw
+    cd ..
+    lib="libuv-v${version}/libuv.a"
+    rm -f src/libuv.a examples/libuv.a test/libuv.a
+    cp "${lib}" src/libuv.a
+    cp "${lib}" examples/libuv.a
+    cp "${lib}" test/libuv.a
+fi
